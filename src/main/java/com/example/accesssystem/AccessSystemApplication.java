63c603154a5d294bd844638f8de0f04ff1ca524f
Kulemin <kulemin86@bk.ru>
2021-04-17 18:12:24 +0400

    started3

---
+++ src/main/java/com/example/accesssystem/AccessSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.accesssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/accesssystem/AccessSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accesssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
